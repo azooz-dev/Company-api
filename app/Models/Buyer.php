203417a1ab7b776a5ffeb542be4f66aa44a799a6
Azooz101ezeo <asooma2005asooma@gmail.com>
2024-06-03 20:41:22 +0300

    creating models with migrations and controllers with specific resources and routes

---
+++ app/Models/Buyer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+
+class Buyer extends User
+{
+    use HasFactory;
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Buyer\BuyerController;
+use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Seller\SellerController;
+use App\Http\Controllers\Transaction\TransactionController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +19,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/*
+| Seller
+*/
+Route::resource('sellers', SellerController::class)->only('index', 'show');
+
+/*
+| Buyer
+*/
+Route::resource('buyers', BuyerController::class)->only('index', 'show');
+
+/*
+| Category
+*/
+Route::resource('categories', CategoryController::class)->except('create', 'edit');
+
+/*
+| Product
+*/
+Route::resource('products', ProductController::class)->only('index', 'show');
+
+/*
+| Transaction
+*/
+Route::resource('transactions', TransactionController::class)->only('index', 'show');
+
+/*
+| User
+*/
+Route::resource('users', UserController::class)->except('create', 'edit');
