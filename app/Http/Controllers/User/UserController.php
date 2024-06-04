8de0fdb065ea2ba794a6b35871ee19217c70f347
Azooz101ezeo <asooma2005asooma@gmail.com>
2024-06-04 03:06:01 +0300

    implementing the show method for UserController

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -31,7 +31,9 @@ class UserController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        return response()->json(['data' => $user], 200);
     }
 
     /**
