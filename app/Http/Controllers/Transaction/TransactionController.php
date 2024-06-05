64cb1914aac8b2e3852633ec652de1616bc5da2b
Azooz101ezeo <asooma2005asooma@gmail.com>
2024-06-05 08:01:57 +0300

    Implementing the operations for transaction

--- app/Http/Controllers/Transaction/TransactionController.php
+++ app/Http/Controllers/Transaction/TransactionController.php
@@ -3,63 +3,28 @@
 namespace App\Http\Controllers\Transaction;
 
 use App\Http\Controllers\ApiController;
-use Illuminate\Http\Request;
+use App\Models\Transaction;
+use App\Traits\ApiResponser;
 
 class TransactionController extends ApiController
 {
+    use ApiResponser;
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
-    }
+        $transactions = Transaction::all();
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+        return $this->showAll($transactions, 200);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Transaction $transaction)
     {
-        //
+        return $this->showOne($transaction, 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
