addd5bc98b3c2363690faf55013ae45197276518
Azooz101ezeo <asooma2005asooma@gmail.com>
2024-06-05 07:59:08 +0300

    Implementing the operations for product

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -3,63 +3,28 @@
 namespace App\Http\Controllers\Product;
 
 use App\Http\Controllers\ApiController;
-use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Traits\ApiResponser;
 
 class ProductController extends ApiController
 {
+    use ApiResponser;
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
-    }
+        $products = Product::all();
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+        return $this->showAll($products, 200);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Product $product)
     {
-        //
+        return $this->showOne($product, 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
